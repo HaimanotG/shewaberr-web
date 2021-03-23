0b7c43854239555c8ffa804329f5ca15d1eaebba
Simonbelete <simonbelete@gmail.com>
2021-03-23 11:32:06 +0300

    Banner text

---
+++ components/molecules/Banner/index.ts
@@ -0,0 +1,8 @@
+import { Banner as Base } from 'components/molecules/Banner/Banner';
+import { BannerTextDesktop } from 'components/molecules/Banner/__text/Banner__text@desktop';
+
+export const Banner = Base;
+
+export {
+    BannerTextDesktop
+}
